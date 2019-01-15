c54d176aedd514523a8ca3b1339a5630a13a696a
Brandon Clayton <bclayton@usgs.gov>
2019-01-15 10:54:51 -0700

    Todo get id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 
 let express = require('express');
 let bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
@@ -28,6 +29,22 @@ app.get('/todos', (req, res) => {
   }).catch((err) => {
     res.status(400).send(err);
   })
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send(`ID [${id}] not valid`);
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send('Todo not found');
+    }
+
+    res.send(todo);
+  }).catch((err) => res.status(400).send('Unable to fetch todo'));
 });
 
 
