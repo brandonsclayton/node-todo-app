8d42a0064ec92b83c94a235620374fd9bb9937e2
Brandon Clayton <bclayton@usgs.gov>
2019-01-15 08:45:28 -0700

    Added update mongo file

---
+++ playground/mongodb-update.js
@@ -0,0 +1,28 @@
+
+// const MongoClient = require('monb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5c3cf4cc7c771e271bda8d13')
+  }, {
+    $set: {
+      name: 'Brandon'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  })
+
+  // db.close();
+});
